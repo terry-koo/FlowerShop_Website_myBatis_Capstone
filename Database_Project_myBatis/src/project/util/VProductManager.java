50448aa548bd127ab9436db92fbaf69d8529d8f6
terry-koo <xpflxhfl94@naver.com>
2020-11-14 00:40:52 +0900

    product메인페이지에 들어가 ArrayList 싱글턴패턴으로 구현

--- Database_Project_myBatis/src/project/util/VProductManager.java
+++ Database_Project_myBatis/src/project/util/VProductManager.java
@@ -1,5 +1,27 @@
 package project.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import mybatis.repository.session.SessionRepository;
+
 public class VProductManager {
+	private static ArrayList<VProduct> productArray = null;
+	private static SessionRepository sessionRepository = new SessionRepository(); 
+	private static List<Object> selectListProduct = sessionRepository.selectVProductArticlePictureLatest();
+	private static int size = selectListProduct.size();
+	
+	private VProductManager() {};
+	
+	public static ArrayList<VProduct> getInstance() {
+		
+		if(productArray == null) {
+			productArray = new ArrayList<>();
+			for(int i=0; i<size; i++) {
+				productArray.add(new VProduct(i));
+			}			
+		}
+		return productArray;
+	}
 
 }
